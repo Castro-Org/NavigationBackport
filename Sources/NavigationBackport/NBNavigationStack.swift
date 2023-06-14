61d2622b74f6d87db505819106c00052dc2f55f7
Ferhat Abdullahoglu <abdullahoglu@sabanciuniv.edu>
2023-06-15 01:18:13 +0300

    wip

--- Sources/NavigationBackport/NBNavigationStack.swift
+++ Sources/NavigationBackport/NBNavigationStack.swift
@@ -4,88 +4,106 @@ import SwiftUI
 @available(iOS, deprecated: 16.0, message: "Use SwiftUI's Navigation API beyond iOS 15")
 /// A replacement for SwiftUI's `NavigationStack` that's available on older OS versions.
 public struct NBNavigationStack<Root: View, Data: Hashable>: View {
-  @Binding var externalTypedPath: [Data]
-  @State var internalTypedPath: [Data] = []
-  @StateObject var path = NavigationPathHolder()
-  @StateObject var pathAppender = PathAppender()
-  @StateObject var destinationBuilder = DestinationBuilderHolder()
-  var root: Root
-  var useInternalTypedPath: Bool
-
-  var content: some View {
-    pathAppender.append = { [weak path] newElement in
-      path?.path.append(newElement)
+    @Binding var externalTypedPath: [Data]
+    @State var internalTypedPath: [Data] = []
+    @StateObject var path = NavigationPathHolder()
+    @StateObject var pathAppender = PathAppender()
+    @StateObject var destinationBuilder = DestinationBuilderHolder()
+    var root: Root
+    var useInternalTypedPath: Bool
+    
+    var content: some View {
+        pathAppender.append = { [weak path] newElement in
+            path?.path.append(newElement)
+        }
+        return NavigationWrapper {
+            Router(rootView: root, screens: $path.path)
+        }
+        .environmentObject(path)
+        .environmentObject(pathAppender)
+        .environmentObject(destinationBuilder)
+        .environmentObject(Navigator(useInternalTypedPath ? $internalTypedPath : $externalTypedPath))
     }
-    return NavigationWrapper {
-      Router(rootView: root, screens: $path.path)
+    
+    public var body: some View {
+        content
+            .onFirstAppear {
+                path.withDelaysIfUnsupported(\.path) {
+                    $0 = externalTypedPath
+                }
+            }
+            .onChange(of: externalTypedPath) { externalTypedPath in
+                path.withDelaysIfUnsupported(\.path) {
+                    $0 = externalTypedPath
+                }
+            }
+            .onChange(of: internalTypedPath) { internalTypedPath in
+                path.withDelaysIfUnsupported(\.path) {
+                    $0 = internalTypedPath
+                }
+            }
+            .onChange(of: path.path) { path in
+                if useInternalTypedPath {
+                    guard path != internalTypedPath.map({ $0 }) else { return }
+                    internalTypedPath = path.compactMap { anyHashable in
+                        if let data = anyHashable.base as? Data {
+                            return data
+                        } else if anyHashable.base is LocalDestinationID {
+                            return nil
+                        }
+                        fatalError("Cannot add \(type(of: anyHashable.base)) to stack of \(Data.self)")
+                    }
+                } else {
+                    guard path != externalTypedPath.map({ $0 }) else { return }
+                    externalTypedPath = path.compactMap { anyHashable in
+                        if let data = anyHashable.base as? Data {
+                            return data
+                        } else if anyHashable.base is LocalDestinationID {
+                            return nil
+                        }
+                        fatalError("Cannot add \(type(of: anyHashable.base)) to stack of \(Data.self)")
+                    }
+                }
+            }
     }
-    .environmentObject(path)
-    .environmentObject(pathAppender)
-    .environmentObject(destinationBuilder)
-    .environmentObject(Navigator(useInternalTypedPath ? $internalTypedPath : $externalTypedPath))
-  }
-
-  public var body: some View {
-    content
-      .onFirstAppear {
-        path.withDelaysIfUnsupported(\.path) {
-          $0 = externalTypedPath
-        }
-      }
-      .onChange(of: externalTypedPath) { externalTypedPath in
-        path.withDelaysIfUnsupported(\.path) {
-          $0 = externalTypedPath
-        }
-      }
-      .onChange(of: internalTypedPath) { internalTypedPath in
-        path.withDelaysIfUnsupported(\.path) {
-          $0 = internalTypedPath
-        }
-      }
-      .onChange(of: path.path) { path in
-        if useInternalTypedPath {
-          guard path != internalTypedPath.map({ $0 }) else { return }
-          internalTypedPath = path.compactMap { anyHashable in
-            if let data = anyHashable.base as? Data {
-              return data
-            } else if anyHashable.base is LocalDestinationID {
-              return nil
-            }
-            fatalError("Cannot add \(type(of: anyHashable.base)) to stack of \(Data.self)")
-          }
-        } else {
-          guard path != externalTypedPath.map({ $0 }) else { return }
-          externalTypedPath = path.compactMap { anyHashable in
-            if let data = anyHashable.base as? Data {
-              return data
-            } else if anyHashable.base is LocalDestinationID {
-              return nil
-            }
-            fatalError("Cannot add \(type(of: anyHashable.base)) to stack of \(Data.self)")
-          }
-        }
-      }
-  }
-
-  public init(path: Binding<[Data]>?, @ViewBuilder root: () -> Root) {
-    _externalTypedPath = path ?? .constant([])
-    self.root = root()
-    useInternalTypedPath = path == nil
-  }
+    
+    public init(path: Binding<[Data]>?, @ViewBuilder root: () -> Root) {
+        _externalTypedPath = path ?? .constant([])
+        self.root = root()
+        useInternalTypedPath = path == nil
+    }
 }
 
 public extension NBNavigationStack where Data == AnyHashable {
-  init(@ViewBuilder root: () -> Root) {
-    self.init(path: nil, root: root)
-  }
+    init(@ViewBuilder root: () -> Root) {
+        self.init(path: nil, root: root)
+    }
 }
 
 public extension NBNavigationStack where Data == AnyHashable {
-  init(path: Binding<NBNavigationPath>, @ViewBuilder root: () -> Root) {
-    let path = Binding(
-      get: { path.wrappedValue.elements },
-      set: { path.wrappedValue.elements = $0 }
-    )
-    self.init(path: path, root: root)
-  }
+    init(path: Binding<NBNavigationPath>, @ViewBuilder root: () -> Root) {
+        let path = Binding(
+            get: { path.wrappedValue.elements },
+            set: { path.wrappedValue.elements = $0 }
+        )
+        self.init(path: path, root: root)
+    }
+    
+    init(path: Binding<NBNavigationPath>, disableAnimations: Binding<Bool>, @ViewBuilder root: () -> Root) {
+        let path = Binding(
+            get: { path.wrappedValue.elements },
+            set: { newValue in
+                if disableAnimations.wrappedValue {
+                    var transaction = Transaction(animation: nil)
+                    transaction.disablesAnimations = true
+                    withTransaction(transaction) {
+                        path.wrappedValue.elements = newValue
+                    }
+                } else {
+                    path.wrappedValue.elements = newValue
+                }
+            }
+        )
+        self.init(path: path, root: root)
+    }
 }
